5c78555f75ee6b0e44c38c6c2c702484c8faa085
timcursedhorizon <tim.cursedhorizon@gmail.com>
2025-11-22 11:36:22 +0100

    Skript Button

--- Assets/Scenes/Scenemanager.cs
+++ Assets/Scenes/Scenemanager.cs
@@ -4,26 +4,35 @@ using UnityEngine.UI; // Für Button
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public Button myButton; // Im Inspector zuweisen
-    public string sceneName; // Name der Zielszene
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (myButton != null)
-        {
-            myButton.onClick.AddListener(LoadTargetScene);
-        }
+
     }
 
-    void LoadTargetScene()
-    {
-        SceneManager.LoadScene(sceneName);
-    }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
+
+
+
+    puplic void GoToGameScene()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+
+    puplic void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
 }
+
+
+
+
+
