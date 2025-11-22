8fb9c1f39721bcd46d2c08a6ebca0d5f60968fd9
jasincursedhorizon-creator <jasin.cursedhorizon@gmail.com>
2025-11-22 11:15:10 +0100

    edited the scenemanagement script

--- Assets/Scenes/Scenemanager.cs
+++ Assets/Scenes/Scenemanager.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Für Szenenwechsel
+using UnityEngine.UI; // Für Button
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public Button myButton; // Im Inspector zuweisen
+    public string sceneName; // Name der Zielszene
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (myButton != null)
+        {
+            myButton.onClick.AddListener(LoadTargetScene);
+        }
+    }
+
+    void LoadTargetScene()
+    {
+        SceneManager.LoadScene(sceneName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
