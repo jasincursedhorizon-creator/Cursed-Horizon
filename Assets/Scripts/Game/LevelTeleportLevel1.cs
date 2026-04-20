ac6f2de7abb265f511cdd11ce6414cf7ecc9424e
timcursedhorizon <tim.cursedhorizon@gmail.com>
2026-04-21 00:28:58 +0200

    LevelTeleport und Moving Background

---
+++ Assets/Scripts/Game/LevelTeleportLevel1.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTeleportLevel1 : MonoBehaviour
+{
+    [SerializeField] private string sceneName = "Level1";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
